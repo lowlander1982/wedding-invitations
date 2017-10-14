2173c91ea86f3fe62a89ed3ef87437208afbedcd
Fonnegra <david.gomez.fonnegra@gmail.com>
2017-10-14 10:10:04 -0500

    Adding admin pages and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -8,13 +9,24 @@ import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { InvitedComponent } from './invited/invited.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'admin/list', component: AdministrationComponent },
+  { path: 'invitation/:id', component: InvitedComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     InvitedComponent,
+    AdministrationComponent,
+    NotFoundComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
--- src/app/invited/invited.component.ts
+++ src/app/invited/invited.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-invited',
   templateUrl: './invited.component.html',
   styleUrls: ['./invited.component.css']
 })
-export class InvitedComponent {
+export class InvitedComponent implements OnInit {
 
-  items: FirebaseListObservable<any[]>;
+  invitation: FirebaseObjectObservable<{
+    family_head:string,
+    email:string,
+    phone:string,
+    address:string,
+    building_name:string,
+    tower:string,
+    apartment:string,
+    city:string,
+    country:string,
+    significant_other:string,
+    children:string[]
+  }>;
 
-  constructor(db: AngularFireDatabase) {
-    this.items = db.list('/invited');
-    // console.log(db);
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private db: AngularFireDatabase
+  ) { }
+
+  ngOnInit() {
+    this.route.paramMap
+      .subscribe((invitationId) => {
+        this.invitation = this.db.object(`/invitations/${invitationId.get('id')}`);
+    });
+  }
+
+  onSubmit() {
+    console.log('entre');
   }
 
 }
